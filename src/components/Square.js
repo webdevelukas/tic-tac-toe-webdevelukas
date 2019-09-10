2ea4e25be42c469a812cec120ffaf358db77995f
Lukas Kreidenweis <lukas@deinklub.de>
2019-09-10 14:00:20 +0200

    add state to square

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 export default function Square(props) {
+  const [state, setState] = React.useState({ value: null });
+
   return (
-    <button className="square" onClick={() => alert(props.value)}>
-      {props.value}
+    <button className="square" onClick={() => setState({ value: "X" })}>
+      {state.value}
     </button>
   );
 }
